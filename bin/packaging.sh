b5563009e2f0c8234834daee4159802092c9d6a2
Thomas Egeling <thomas.egeling@googlemail.com>
2021-01-17 16:59:02 +0100

    Added NamedCredential

--- bin/packaging.sh
+++ bin/packaging.sh
@@ -1,8 +1,8 @@
-sfdx force:package:create --name "Geocoding" --description "Geocoding sample app" --packagetype Unlocked --path force-app --nonamespace --targetdevhubusername devhub
+sfdx force:package:create --name "Eventbrite" --description "Integrate Salesforce with Eventbrite to sync event and attendee data" --packagetype Unlocked --path force-app --nonamespace --targetdevhubusername devhub
 
-sfdx force:package:version:create --package "Geocoding" --path force-app --installationkeybypass --wait 20 --targetdevhubusername devhub
+sfdx force:package:version:create --package "Eventbrite" --path force-app --installationkeybypass --wait 20 --targetdevhubusername devhub
 
-sfdx force:package:version:promote --package Geocoding@2.0.0-1 --targetdevhubusername devhub
+sfdx force:package:version:promote --package Eventbrite@1.0.0-1 --targetdevhubusername devhub
 
 https://login.salesforce.com/packaging/installPackage.apexp?p0=04t5I0000019imrQAA
 https://test.salesforce.com/packaging/installPackage.apexp?p0=04t5I0000019imrQAA
