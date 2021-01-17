28c62d5d57a500b882db987f302fd8681ed27fd6
Thomas Egeling <thomas.egeling@googlemail.com>
2021-01-17 16:43:44 +0100

    Basic project setup

--- bin/orgInit.sh
+++ bin/orgInit.sh
@@ -6,9 +6,7 @@ if [ "$#" -eq 1 ]; then
   DURATION=$1
 fi
 
-sfdx force:org:create -a geotesting -s -f config/project-scratch-def.json -d $DURATION
+sfdx force:org:create -a eventbrite -s -f config/project-scratch-def.json -d $DURATION
 sfdx force:source:push
-sfdx force:user:permset:assign -n Geocoding
-sfdx force:data:tree:import --plan ./data/Anlagen__c-plan.json
-sfdx force:org:open -p /lightning/n/Geocoding_Search
+sfdx force:user:permset:assign -n Eventbrite
 echo "Org is set up"
