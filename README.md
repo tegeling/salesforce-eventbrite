fcf64b56b312008d11ded69d7faf42a13cdf8f09
Thomas Egeling <thomas.egeling@googlemail.com>
2021-01-18 09:14:41 +0100

    Prep packaging

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Salesforce Eventbrite Integration
 
+DRAFT Mode - currently not deployable
+
 This repository contain Salesforce SFDX resources to integrate Salesforce with Eventbrite and retrieve event and attendee records.
 It retrieves records from Eventbrite via Rest API and stores records in Salesforce custom objects.
+
+Installation link for Sandbox Orgs
+https://test.salesforce.com/packaging/installPackage.apexp?p0=04t5I000001mskPQAQ
+
+Installation link for Production Orgs
+https://login.salesforce.com/packaging/installPackage.apexp?p0=04t5I000001mskPQAQ
--- bin/packaging.sh
+++ bin/packaging.sh
@@ -2,7 +2,7 @@ sfdx force:package:create --name "Eventbrite" --description "Integrate Salesforc
 
 sfdx force:package:version:create --package "Eventbrite" --path force-app --installationkeybypass --wait 20 --targetdevhubusername devhub
 
-sfdx force:package:version:promote --package Eventbrite@1.0.0-1 --targetdevhubusername devhub
+sfdx force:package:version:promote --package Eventbrite@0.1.0-1 --targetdevhubusername devhub
 
 https://login.salesforce.com/packaging/installPackage.apexp?p0=04t5I0000019imrQAA
 https://test.salesforce.com/packaging/installPackage.apexp?p0=04t5I0000019imrQAA
