59c686d21ff7259326e74a9d4ec900eb8f5af55d
Thomas Egeling <thomas.egeling@googlemail.com>
2021-01-18 15:44:58 +0100

    Unlocked package released

--- README.md
+++ README.md
@@ -4,13 +4,11 @@
 
 [![codecov](https://codecov.io/gh/tegeling/salesforce-eventbrite/branch/main/graph/badge.svg?token=G67C6FTWLJ)](https://codecov.io/gh/tegeling/salesforce-eventbrite)
 
-DRAFT Mode - currently not deployable
-
 This repository contain Salesforce SFDX resources to integrate Salesforce with Eventbrite and retrieve event and attendee records.
 It retrieves records from Eventbrite via Rest API and stores records in Salesforce custom objects.
 
 Installation link for Sandbox Orgs
-https://test.salesforce.com/packaging/installPackage.apexp?p0=04t5I000001mskPQAQ
+https://test.salesforce.com/packaging/installPackage.apexp?p0=04t5I000001mskjQAA
 
 Installation link for Production Orgs
-https://login.salesforce.com/packaging/installPackage.apexp?p0=04t5I000001mskPQAQ
+https://login.salesforce.com/packaging/installPackage.apexp?p0=04t5I000001mskjQAA
--- bin/packaging.sh
+++ bin/packaging.sh
@@ -1,8 +1,8 @@
 sfdx force:package:create --name "Eventbrite" --description "Integrate Salesforce with Eventbrite to sync event and attendee data" --packagetype Unlocked --path force-app --nonamespace --targetdevhubusername devhub
 
-sfdx force:package:version:create --package "Eventbrite" --path force-app --installationkeybypass --wait 20 --targetdevhubusername devhub
+sfdx force:package:version:create --package "Eventbrite" --path force-app --installationkeybypass --wait 20 --codecoverage --targetdevhubusername devhub
 
-sfdx force:package:version:promote --package Eventbrite@0.1.0-1 --targetdevhubusername devhub
+sfdx force:package:version:promote --package Eventbrite@0.1.0-3 --targetdevhubusername devhub
 
 https://login.salesforce.com/packaging/installPackage.apexp?p0=04t5I0000019imrQAA
 https://test.salesforce.com/packaging/installPackage.apexp?p0=04t5I0000019imrQAA
