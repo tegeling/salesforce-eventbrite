7fa0c8faf4eb08e7cb4eac5b3139979f6dfa8eb8
Thomas Egeling <thomas.egeling@googlemail.com>
2021-01-21 15:48:23 +0100

    Reset meetadata deployment for proper testing

--- bin/orgInit.sh
+++ bin/orgInit.sh
@@ -6,7 +6,7 @@ if [ "$#" -eq 1 ]; then
   DURATION=$1
 fi
 
-sfdx force:org:create -a eventbrite -s -f config/project-scratch-def.json -d $DURATION
+sfdx force:org:create -a eventbrite2 -s -f config/project-scratch-def.json -d $DURATION
 sfdx force:source:push
 sfdx force:user:permset:assign -n Eventbrite
 sfdx force:org:open -p /lightning/o/EBEvent__c/home
